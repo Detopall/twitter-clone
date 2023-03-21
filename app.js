ee94f20b759b05ae8ec2e60f0a1379ee45c3b6b9
denis.topallaj <denis.topallaj@student.howest.be>
2023-03-21 14:40:01 +0100

    added routes file and folder, added public file and folder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -10,7 +11,5 @@ const server = app.listen(PORT, () => console.log("server listening on port: ", 
 app.engine('.hbs', exphbs.engine({defaultLayout: "main", extname: ".hbs",  partialsDir: __dirname + '/views/partials'}));
 app.set('view engine', '.hbs');
 
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use('/', require('./routes/routes'));
+app.use(express.static(path.join(__dirname, 'public')));
