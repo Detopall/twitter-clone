daba7381b86c4a962ce3ba6378f50f3bb46461e5
denis.topallaj <denis.topallaj@student.howest.be>
2023-03-21 15:15:07 +0100

    added the route to login, made sure that all users that are not loggin are redirected to the /login page. A partial has been made for this to reuse in the sign up page (niy)

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const middleware = require("../middleware");
 
 
-router.get("/", async (req, res) => {
+router.get("/", middleware.requireLogin, async (req, res) => {
 	const payload = {
 		"pageTitle": "Home"
 	};
 	
 	res.render("home", payload);
+});
+
+
+router.get("/login", async (req, res) => {
+	const payload = {
+		"pageTitle": "Login"
+	};
+	
+	res.render("login", payload);
 });
 
 module.exports = router;
