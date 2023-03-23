23d5c628f33fea467863e92802d8a196863701a6
denis.topallaj <denis.topallaj@student.howest.be>
2023-03-23 13:01:55 +0100

    creating a post works, is added to the db (in plain txt) and will be send to the user.

--- controller/post-controller.js
+++ controller/post-controller.js
@@ -1,9 +1,19 @@
 "use strict";
 
+const TwitterPost = require("../models/TwitterPost");
+
 exports.sendPost = async (req, res) => {
 	if (!req.body.content) {
 		return res.sendStatus(400);
 	}
-	console.log(req.body);
-	res.send({data: "This works"});
+	const post = new TwitterPost({
+		content: req.body.content,
+		postedBy: req.session.user
+	});
+	try {
+		await post.save();
+		return res.send(post);
+	} catch(err){
+		return res.sendStatus(400);
+	}
 }
