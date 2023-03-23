efc341a43403efa77bd7eb48f76b00da602133ad
denis.topallaj <denis.topallaj@student.howest.be>
2023-03-23 15:38:17 +0100

    added dates =>  ...ago

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+const DURATION_IN_SECONDS = {
+	epochs: ['year', 'month', 'day', 'hour', 'minute'],
+	year: 31536000,
+	month: 2592000,
+	day: 86400,
+	hour: 3600,
+	minute: 60
+};
+
 document.addEventListener("keyup", (e) => {
 	if (e.target.matches("#post-textarea")){
 		const textarea = document.querySelector("#post-textarea");
@@ -69,7 +78,7 @@ function displayPost(postData){
 
 function createPostHtml(postData){
 	const displayName = `${postData.postedBy.firstname} ${postData.postedBy.lastname}`;
-	const timestamp = "TO DO LATER";
+	const timestamp = timeSince(postData.createdAt);
 
 	return `
 			<div class="post">
@@ -112,3 +121,30 @@ function createPostHtml(postData){
 			</div>
 			`
 }
+
+
+function timeSince(date) {
+  let seconds = Math.floor((new Date() - new Date(date)) / 1000);
+  let duration = getDuration(seconds);
+  if (duration === undefined){
+	return "just now";
+  }
+  let suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
+  return duration.interval + ' ' + duration.epoch + suffix + " ago";
+}
+
+
+function getDuration(seconds) {
+	let epoch, interval;
+	for (let i = 0; i < DURATION_IN_SECONDS.epochs.length; i++) {
+	  epoch = DURATION_IN_SECONDS.epochs[i];
+	  interval = Math.floor(seconds / DURATION_IN_SECONDS[epoch]);
+	  if (interval >= 1) {
+		return {
+		  interval: interval,
+		  epoch: epoch
+		};
+	  }
+	}
+}
+  
