8425e56bda60ea48745f3022feb1fedec33466a7
denis.topallaj <denis.topallaj@student.howest.be>
2023-03-23 22:25:26 +0100

    added the post-retweet.js file

---
+++ public/js/post-retweet.js
@@ -0,0 +1,31 @@
+"use strict";
+
+document.addEventListener("click", async (e) => {
+	if (!e.target.closest(".retweet-button")) return;
+
+	const retweetButton = e.target.closest(".retweet-button");
+	const postId = getRootIdElement(retweetButton);
+	if (!postId) return;
+
+	const postData = await sendRetweet(postId);
+	if (!postData) return;
+
+	console.log(postData);
+	//retweetButton.querySelector("span").innerHTML = `${postData.likes.length}` || "";
+
+	// if (postData.likes.includes(USER_LOGGED_IN._id)){
+	// 	retweetButton.classList.add("active");
+	// } else {
+	// 	retweetButton.classList.remove("active");
+	// }
+});
+
+async function sendRetweet(postId){
+	try {
+		const response = await fetch(`/api/posts/${postId}/retweet`, getOptionsPost({}, "POST"));
+		const jsonData = await response.json();
+		return jsonData;
+	} catch (err) {
+		console.error("Something went wrong: ", err);
+	}
+}
